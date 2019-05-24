070fa8f06b912279ef8a394e06fe27f4b7278678
sharabinth <maha@hashicorp.com>
2019-05-24 19:18:05 +1000

    initial

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "ip" {
+  value = "${aws_eip.ptfe-demo.public_ip}"
+}
+
+output "private_ip" {
+  value = "${aws_eip.ptfe-demo.private_ip}"
+}
+
+output "db_password" {
+  value = "${random_pet.db_pwd.id}"
+}
